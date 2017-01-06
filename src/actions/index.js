1cf017e88540e60f23ba126b0b6440e2f3f8322d
Jorybraun <braunjory@gmail.com>
2017-01-05 20:46:40 -0500

    adds fetchweater action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
-import API_KEY from '../../apikey';      
+import API_KEY from '../../apikey'; 
+const ROOT_URL `api.openweathermap.org/data/2.5/forecast?appid=${ API_KEY }`;      
+
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+// this is an action creator
+export function fetchWeather() {
+  // action creators allways have a type
+  return {
+    type: FETCH_WEATHER
+  };
+}
