fcfd0df5ce57def572eae633a0a79658cb72f6ae
Jorybraun <braunjory@gmail.com>
2017-01-05 20:55:32 -0500

    it adds axios and adds string for query

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+
+
 import API_KEY from '../../apikey'; 
 const ROOT_URL `api.openweathermap.org/data/2.5/forecast?appid=${ API_KEY }`;      
 
@@ -5,9 +8,13 @@ const ROOT_URL `api.openweathermap.org/data/2.5/forecast?appid=${ API_KEY }`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 // this is an action creator
-export function fetchWeather() {
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},ca`;
+  const request = axios.get(url);
+
   // action creators allways have a type
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
